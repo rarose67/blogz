b677c6fa8a7c8a6e08dba7ea00c4d3e9e8064490
rarose67 <robertrosestl67@gmail.com>
2018-04-20 18:26:55 -0500

    added Hashing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, redirect, render_template, session, flash, url_for
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
+from hashutils import make_pw_hash, check_pw_hash
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -30,12 +31,12 @@ class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120))
+    pw_hash = db.Column(db.String(120))
     blogs = db.relationship("Blog", backref="owner")
 
     def __init__(self, username, password):
         self.username = username
-        self.password = password
+        self.pw_hash = make_pw_hash(password)
 
 @app.route('/')
 def index():
@@ -109,7 +110,7 @@ def login():
         #query the database to find the user record.
         user = User.query.filter_by(username=username).first()
         #if the user exsist, and the password matches the one in the database, log the user in.
-        if (user and user.password == password):
+        if (user and (check_pw_hash(password, user.pw_hash))):
             session['username'] = username
             flash("Logged in")
             return redirect("/new-post")
